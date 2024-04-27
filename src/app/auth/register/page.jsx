f1136a88a833e5a7c6953051052848b4460b3089
Rafael Díaz <rafandpufff@gmail.com>
2024-04-27 18:57:49 +0200

    Add register form validation

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -2,7 +2,11 @@
 import { useForm } from "react-hook-form"
 
 function RegisterPage() {
-  const { register, handleSubmit } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
   const onSubmit = handleSubmit((data) => {
     console.log(data)
   })
@@ -12,41 +16,72 @@ function RegisterPage() {
       <form action="" onSubmit={onSubmit}>
         <h1 className="text-slate-200 font-bold text-4xl mb-4">Register</h1>
 
-        <label htmlFor="Username" className="text-slate-500 mb-2 block text-sm">
+        <label htmlFor="username" className="text-slate-500 mb-2 block text-sm">
           Username
         </label>
         <input
           type="text"
-          {...register("username", { required: true })}
+          {...register("username", {
+            required: { value: true, message: "Username required" },
+          })}
           className="p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full"
+          placeholder="user"
         />
-        <label htmlFor="Email" className="text-slate-500 mb-2 block text-sm">
+        {errors.username && (
+          <span className="text-red-500 text-xs">
+            {errors.username.message}
+          </span>
+        )}
+
+        <label htmlFor="email" className="text-slate-500 mb-2 block text-sm">
           Email
         </label>
         <input
           type="email"
-          {...register("email", { required: true })}
+          {...register("email", {
+            required: { value: true, message: "Email required" },
+          })}
           className="p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full"
+          placeholder="user@email.com"
         />
-        <label htmlFor="Password" className="text-slate-500 mb-2 block text-sm">
+        {errors.email && (
+          <span className="text-red-500 text-xs">{errors.email.message}</span>
+        )}
+        <label htmlFor="password" className="text-slate-500 mb-2 block text-sm">
           Password
         </label>
         <input
           type="password"
-          {...register("password", { required: true })}
+          {...register("password", {
+            required: { value: true, message: "Password required" },
+          })}
           className="p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full"
+          placeholder="****"
         />
+        {errors.password && (
+          <span className="text-red-500 text-xs">
+            {errors.password.message}
+          </span>
+        )}
         <label
-          htmlFor="ConfirmPassword"
+          htmlFor="confirmPassword"
           className="text-slate-500 mb-2 block text-sm"
         >
           Confirm Password
         </label>
         <input
-          type="confirmPassword"
-          {...register("confirmPassword", { required: true })}
+          type="password"
+          {...register("confirmPassword", {
+            required: { value: true, message: "Confirm password required" },
+          })}
           className="p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full"
+          placeholder="****"
         />
+        {errors.confirmPassword && (
+          <span className="text-red-500 text-xs">
+            {errors.confirmPassword.message}
+          </span>
+        )}
         <button className="bg-blue-500 text-white p-3 rounded-lg w-full mt-2">
           Register
         </button>
