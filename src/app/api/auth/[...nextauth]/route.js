b17bee0c2f69bcda1c061eb2af61065de839b0e5
Rafael Díaz <rafandpufff@gmail.com>
2024-05-04 13:58:43 +0200

    Add nextauth login

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import db from "@/app/libs/db";
+import bcrypt from "bcrypt";
+
+const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: " Email", type: "text", placeholder: "email" },
+                password: { label: " Password", type: "password" },
+
+            }, 
+            async authorize(credentials, req) {
+
+                const userFound = await db.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
+
+                if (!userFound) {
+                    return null
+                }
+
+                const matchPassword = await bcrypt.compare(credentials.password, userFound.password)
+
+                if (matchPassword) {
+                    return null
+                }
+
+                return {
+                    id: userFound.id,
+                    name: userFound.username,
+                    email: userFound.email
+                }
+            }
+        })
+    ]
+}
+
+
+const handler = NextAuth(authOptions) 
+
+export {handler as GET, handler as POST}
